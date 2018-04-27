0d74929cf584efdf46b70222b155b5b8f897a24c
Matt Jones <matt.jones@referencepoint.co.uk>
2018-04-27 16:15:11 +0100

    Buildout

--- src/Domain/Features/Job/Create.cs
+++ src/Domain/Features/Job/Create.cs
@@ -1,11 +1,30 @@
 ﻿namespace Domain.Features.Job
 {
     using System;
+    using System.Linq;
+    using System.Linq.Expressions;
     using System.Threading.Tasks;
     using DataModel;
     using DataModel.Entities;
+    using FluentValidation;
     using MediatR;
     using Pipeline;
+
+    public abstract class EntityExists<T, TEntity> : AbstractValidator<T>
+        where TEntity : class, IEntity
+    {
+        readonly SchedulingDbContext db;
+
+        protected EntityExists(
+            Expression<Func<T, Guid>> selector,
+            SchedulingDbContext db)
+        {
+            this.db = db;
+
+            RuleFor(selector)
+                .Must(guid => { return db.Set<TEntity>().Any(y => y.Id == guid); });
+        }
+    }
 
     public class Create
     {
@@ -14,6 +33,22 @@
             public Guid StationId { get; set; }
             public DateTime Start { get; set; }
             public DateTime End { get; set; }
+        }
+
+        public class Validation : AbstractValidator<Command>
+        {
+            public Validation()
+            {
+                RuleFor(x => x.End)
+                    .GreaterThan(x => x.Start);
+            }
+        }
+
+        public class StationExists : EntityExists<Command, Station>
+        {
+            public StationExists(SchedulingDbContext db) : base(x => x.StationId, db)
+            {
+            }
         }
 
         public class Handler : AsyncRequestHandler<Command, CommandResult>
